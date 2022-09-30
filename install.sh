8dbb04e71759c627c5c78a6d09a79227461ce660
Akergez <ragusseven@gmail.com>
2022-09-30 10:18:11 +0500

    Initial commit

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-if [[ -d /usr/share/backgrounds/Dynamic_Wallpapers ]]
+if [[ -d ~/.local/share/backgrounds/Dynamic_Wallpapers ]]
 then 
-	sudo rm -r /usr/share/backgrounds/Dynamic_Wallpapers
+	rm -r ~/.local/share/backgrounds/Dynamic_Wallpapers
 	echo "Cleaning up"
 fi
 
 echo "Installing wallpapers..."
-sudo mkdir -p /usr/share/backgrounds/
-sudo mkdir -p /usr/share/gnome-background-properties/ 
-sudo cp -r $(pwd)/Dynamic_Wallpapers /usr/share/backgrounds/Dynamic_Wallpapers
-sudo cp $(pwd)/xml/* /usr/share/gnome-background-properties/
+mkdir -p ~/.local/share/backgrounds/
+mkdir -p ~/.local/share/gnome-background-properties/
+cp -r $(pwd)/Dynamic_Wallpapers ~/.local/share/backgrounds/Dynamic_Wallpapers
+cp $(pwd)/xml/* ~/.local/share/gnome-background-properties/
+python3 $(pwd)/install_variants.py
+
 echo "Wallpapers has been installed. Enjoy setting them as your desktop background!"
